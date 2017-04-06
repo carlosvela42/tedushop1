966620d61fcb26accec6f5f4f0150e8696982fb1
DESKTOP-UIIUOHD\tung <carlosvela42@gmail.com>
2017-04-06 17:57:20 +0700

    add domain entities for project

---
+++ TeduShop/TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TeduShop.Model.Models
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { set; get; }
+        string CreatedBy { set; get; }
+        DateTime? UpdatedDate { set; get; }
+        string UpdatedBy { set; get; }
+
+        string MetaKeyword { get; set; }
+        string MetaDescription { get; set; }
+
+        bool Status { get; set; }
+    }
+}
