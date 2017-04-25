1c9183821dede00609c8f45ed6509722da7614d9
tung <carlosvela42@gmail.com>
2017-04-26 00:03:39 +0700

    automapper for web

---
+++ TeduShop/TeduShop.Web/TeduShop.Web/Infrastructure/Extensions/EntityExtensions.cs
@@ -0,0 +1,61 @@
+﻿using TeduShop.Model.Models;
+using TeduShop.Web.Models;
+
+namespace TeduShop.Web.Infrastructure.Extensions
+{
+    public static class EntityExtensions
+    {
+        public static void UpdatePostCategory(this PostCategory postCategory, PostCategoryViewModel postCategoryVm)
+        {
+            postCategory.ID = postCategoryVm.ID;
+            postCategory.Name = postCategoryVm.Name;
+            postCategory.Alias = postCategoryVm.Alias;
+            postCategory.Description = postCategoryVm.Description;
+            postCategory.ParentID = postCategoryVm.ParentID;
+            postCategory.DisplayOrder = postCategoryVm.DisplayOrder;
+            postCategory.Image = postCategoryVm.Image;
+            postCategory.HomeFlag = postCategoryVm.HomeFlag;
+            postCategory.CreatedDate = postCategoryVm.CreatedDate;
+            postCategory.CreatedBy = postCategoryVm.CreatedBy;
+            postCategory.UpdatedDate = postCategoryVm.UpdatedDate;
+            postCategory.UpdatedBy = postCategoryVm.UpdatedBy;
+            postCategory.MetaKeyword = postCategoryVm.MetaKeyword;
+            postCategory.MetaDescription = postCategoryVm.MetaDescription;
+            postCategory.Status = postCategoryVm.Status;
+        }
+
+        public static void UpdatePostTag(this PostTag postTag, PostTagViewModel postTagVm)
+        {
+            postTag.PostID = postTagVm.PostID;
+            postTag.TagID = postTagVm.TagID;
+        }
+
+        public static void UpdatePost(this Post post, PostViewModel postVm)
+        {
+            post.ID = postVm.ID;
+            post.Name = postVm.Name;
+            post.Alias = postVm.Alias;
+            post.CategoryID = postVm.CategoryID;
+            post.Image = postVm.Image;
+            post.Description = postVm.Description;
+            post.Content = postVm.Content;
+            post.HomeFlag = postVm.HomeFlag;
+            post.HotFlag = postVm.HotFlag;
+            post.ViewCount = postVm.ViewCount;
+            post.CreatedDate = postVm.CreatedDate;
+            post.CreatedBy = postVm.CreatedBy;
+            post.UpdatedDate = postVm.UpdatedDate;
+            post.UpdatedBy = postVm.UpdatedBy;
+            post.MetaKeyword = postVm.MetaKeyword;
+            post.MetaDescription = postVm.MetaDescription;
+            post.Status = postVm.Status;
+    }
+
+        public static void UpdateTag(this Tag postTag, TagViewModel tagVm)
+        {
+            postTag.ID = tagVm.ID;
+            postTag.Name = tagVm.Name;
+            postTag.Type = tagVm.Type;
+        }
+    }
+}
