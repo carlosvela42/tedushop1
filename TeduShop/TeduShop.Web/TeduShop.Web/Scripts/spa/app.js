598f11e6609338a0d4ab1c80888f70b506a62ffa
tung <carlosvela42@gmail.com>
2017-05-10 06:44:33 +0700

    what is scope and rootscope?

---
+++ TeduShop/TeduShop.Web/TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,15 @@
+﻿var myApp = angular.module('myModule', []);
+
+myApp.controller("myController", myController);
+myApp.controller("studentController", studentController);
+//myController.$inject = ['$scope'];
+
+//declare
+function myController($rootScope,$scope) {
+    $rootScope.message = "This is my message from Controller"
+}
+
+//declare
+function studentController($rootScope, $scope) {
+    $scope.message = "This is my message from Student"
+}
