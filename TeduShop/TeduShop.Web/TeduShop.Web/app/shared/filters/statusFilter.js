4d67dbf4cc9fa264b48c45a9f33bba5fe4402322
tung <carlosvela42@gmail.com>
2017-05-30 07:39:34 +0700

    add filter status and format data in list

---
+++ TeduShop/TeduShop.Web/TeduShop.Web/app/shared/filters/statusFilter.js
@@ -0,0 +1,13 @@
+﻿(function (app) {
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input) {
+                return 'Kích hoạt';
+            }
+            else {
+                return 'Khóa';
+            }            
+        }
+    });
+
+})(angular.module('tedushop.common'));
